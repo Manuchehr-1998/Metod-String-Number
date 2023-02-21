5dc3582fe664f1399719c5612a27478a3bc2f166
Manuchehr-1998 <120194005+Manuchehr-1998@users.noreply.github.com>
2023-02-21 11:28:49 +0500

    asdassad

--- app.js
+++ app.js
@@ -10,6 +10,14 @@
 // let part = str.substring(7);
 // console.log(part);
 // metod String substr()
-let str = "Apple, Banana, Kiwi";
-let part = str.substr(-4);
-console.log(part);
+// let str = "Apple, Banana, Kiwi";
+// let part = str.substr(-4);
+// console.log(part);
+// metod replace()
+// let text = "Please visit Microsoft!";
+// let newText = text.replace("Microsoft", "W3Schools");
+// console.log(newText);
+// metod replaceAll()
+let text = "I love cats. Cats are very easy to love. Cats are very popular.";
+console.log(text.replaceAll("Cats", "Dogs"));
+console.log(text.replaceAll("cats", "dogs"));
